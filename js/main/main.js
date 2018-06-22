5e21919eaabfe4f4a003f50ff1efd3ef5db655e7
kkodu <nawy1130@gmail.com>
2018-06-22 18:41:49 +0900

    post contents preloader update

--- js/main/main.js
+++ js/main/main.js
@@ -74,17 +74,17 @@ let getFbText = function(feed, count) {
       ${feed.picture ? hasPicture(feed.picture, feed.picture_link, feed.source) : ""}
       ${feed.source ? hasSource(feed.source, feed.picture, feed.picture_link) : ""}
       <div class="card-footer text-muted ovfl">
-          ${feed.likes ? 
+          ${feed.likes ?
             `<div class="likes">
               <img src="../../images/fb-like-icon.jpg" width="20px" height="20px" alt=""><span class="txt-custom">${feed.likes}</span>
             </div>`
             : ''
           }
-          ${feed.comments ? 
+          ${feed.comments ?
             `<div class="comments ${feed.storyid} comments-${count}" data-toggle="modal" data-target="#exampleModalLong">
               <img src="../../images/fb-comment-icon.jpg" width="20px" height="20px" alt="">
               <span class="txt-custom">${feed.comments}</span>
-            </div>` 
+            </div>`
             : ''
           }
           <span class="txt-custom text-muted pull-right">
@@ -96,10 +96,29 @@ let getFbText = function(feed, count) {
   return fbText;
 }
 
+let post = `
+  <li class="card mb-4 post-loader">
+    <div class="post-header">
+      <div class="profile-photo">
+
+      </div>
+      <div class="profile-details">
+        <span class="bar bar-name"></span>
+        <span class="bar bar-date"></span>
+      </div>
+    </div>
+    <div class="post-content">
+      <span class="bar bar-message"></span>
+      <span class="bar bar-message"></span>
+      <span class="bar bar-message"></span>
+    </div>
+  </li>
+`;
+
 let appendByType = function(contents, feed, count) {
   if (feed.from > 0 && feed.from < 7) {
     contents.append(getFbText(feed, count));
-  } 
+  }
   // else {
   //   contents.append(`
   //     <li class="card mb-4">
@@ -113,14 +132,17 @@ let appendByType = function(contents, feed, count) {
 
 // 초기 온 로드 렌더링
 $(() => {
-  preloader('show'); 
+  preloader('show');
   // 전체 피드들 렌더링
   fn = (result) => {
     primaryFeed = result[0];
     for(let i=0; i<result.length; i++) {
       appendByType(allContents, result[i]);
     }
-    preloader('hide'); 
+    preloader('hide');
+
+    allContents.append(post);
+
     // 어떻게 고치니~~
     showMoreText();
     $(".comments").on('click', handler);
@@ -152,8 +174,8 @@ $(() => {
     makeRankingContent(result);
   }
   AJAX.reqrankingslide('https://syuproject.herokuapp.com/ranking/rank-slide', fn);
-  
-  // 삼대전: 2, 대나무숲: 1, 삼육대학교: 6, 총학: 3, 연합: 4, 컴과: 5 
+
+  // 삼대전: 2, 대나무숲: 1, 삼육대학교: 6, 총학: 3, 연합: 4, 컴과: 5
   var ranktypes = [2, 1, 6, 3, 4, 5];
   fn = function(result, type) {
     makeRankingType(result, type);
@@ -164,13 +186,20 @@ $(() => {
 });
 
 let scrollFlag = true;
-const preloader = function(result) {
-  if(result === 'show') {
+const preloader = function (result) {
+  if (result === 'show') {
     $('.bouncing-loader').css('visibility', 'visible');
   } else {
     $('.bouncing-loader').css('visibility', 'hidden');
   }
 };
+const postloader = function (result) {
+  if (result === 'show') {
+    $('.post-loader').show();
+  } else {
+    $('.post-loader').hide();
+  }
+}
 
 // 무한 스크롤링 시 렌더링
 $('.slide-1').scroll(function(e) {
@@ -182,14 +211,14 @@ $('.slide-1').scroll(function(e) {
       scrollFlag = false; // 접근 제한 플래그
       console.log('[loading..] more feeds');
 
-      // => 로딩 아이콘 보여주기
-
+      postloader('show'); // => 로딩 아이콘 보여주기
       setTimeout(function() {
         fn = (result) => {
-          // 1.2초 후, 로딩 아이콘 숨김
+          postloader('hide'); // 1.2초 후, 로딩 아이콘 숨김
           for(let i=0; i<result.length; i++) {
             appendByType(allContents, result[i], allPageCount);
           }
+          allContents.append(post);
           showMoreText(); // more(계속읽기) 적용
           $(`.comments-${allPageCount}`).on('click', handler);
           scrollFlag = true; // flag 원래대로 바꾸기
